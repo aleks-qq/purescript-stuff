3fd6b9f10efb59be933fa129786062b725eebee4
Aleksi Skantz <aleksi.skantz@siili.com>
2018-07-01 16:29:16 +0300

    fixd

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,32 @@
 module Main where
 
 import Prelude
-import Effect (Effect)
-import Effect.Console (log)
 
-main :: Effect Unit
-main = do
-  log "Hello sailor!"
+import Effect.Uncurried (mkEffectFn1)
+import React.Basic as R
+import React.Basic.DOM as RB
+
+type ExampleProps =
+  { label :: String
+  }
+
+type ExampleState =
+  { counter :: Int
+  }
+
+example :: R.ReactComponent ExampleProps
+example = R.react
+  { initialState
+  , render
+  , displayName: "something"
+  , receiveProps: \_ _ _ -> pure unit
+  }
+  where
+    initialState = { counter: 0 }
+    render { label } { counter } setState =
+      RB.button
+        { onClick: mkEffectFn1 \_ -> do
+            setState _ { counter = counter + 1 }
+        , children: [ RB.text (label <> ": " <> show counter)]
+        }
+        
